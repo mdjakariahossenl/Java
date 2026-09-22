2cf88c4b54384012053f2c800dcc79eaad9d76cd
MD JAKARIA HOSSEN <mdjakariahossen78@gmail.com>
2026-09-22 12:25:59 +0600

    Add files via upload

---
+++ DoubletoInt.java
@@ -0,0 +1,10 @@
+public class DoubletoInt{
+    public static void main(String[]args){
+        double myNum = 50.0d;
+        int myInt =(int) myNum;
+
+        System.out.println("Entered double value" + ":" + myNum);
+        System.out.println("Converted intiger value" + ":" + myInt);
+    }
+    
+}
